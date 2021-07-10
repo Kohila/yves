6c3a35fb3d258b1cba26fe2aec0ed975622ddadd
Kohila <kohilaice@gmail.com>
2021-07-10 07:13:51 +0000

    Further RSS module fixes

--- utils/rss.js
+++ utils/rss.js
@@ -7,13 +7,15 @@ const { RSS_DIR, DCRC_API, DCRC_WS } = require('../config')
 const log = console.log
 
 const generateRSS = async (story, jwt) => {
-	var { data:storyData } = await axios.get(`${DCRC_API}/stories?name=${story}`, jwt)
+	
+	const storyRoot = story.split('/', 1)
+	
+	var { data:storyData } = await axios.get(`${DCRC_API}/stories?name=${storyRoot}`, jwt)
 	const { data:pages } = await axios.get(`${DCRC_API}/pages?slug_contains=${story}&_sort=pageNumber:DESC&_limit=50`)
 	
 	// Edge case handling for Vast Error
-	const storyRSS = story
 	story = (story == 'vast-error') ? 'vasterror' : story
-	
+		
 	if(!fs.existsSync(RSS_DIR)) {
 	  fs.mkdir(RSS_DIR, {recursive: true}, (err) => {if(err) throw err})
 	}
@@ -41,17 +43,9 @@ const generateRSS = async (story, jwt) => {
 		})
 	}
 	
-	/*let feed = `<?xml version="1.0" encoding="UTF-8" ?>\n<rss version="2.0">\n<channel>\n  <title>${storyData.title}</title>\n  <link>${DCRC_WS}/${story}</link>\n  <description>${storyData.description}</description>\n  <lastBuildDate>${moment(now)}</lastBuildDate>\n`
-	
-	for(let page of pages) {
-	  feed += `  <item>\n    <title>${page.pageNumber}</title>\n    <description><![CDATA[${page.command}]]></description>\n    <link>${DCRC_WS}/${story}/${page.pageNumber}</link>\n    <pubDate>${moment(page.published_at)}</pubDate>\n    <guid>${page.slug}</guid>\n    </item>\n`
-	}
-	
-	feed += `</channel>\n</rss>`*/
-	
 	log(feed.xml({ indent: true }))
 	
-	fs.writeFileSync(`${RSS_DIR}/${storyRSS}.xml`, feed.xml({ indent: true }))
+	fs.writeFileSync(`${RSS_DIR}/${storyRoot}.xml`, feed.xml({ indent: true }))
 	console.log(`RSS Feed created on ${now}`.green)
 }
 	
